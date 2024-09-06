85eb85f33ea2eea1f4f59f24a908cb284b140ea6
Laurent <saintlaurent414@gmail.com>
2024-09-06 18:15:52 +0100

    Tailwind react

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -7,7 +7,7 @@ const UserProfile = ({ user }) => {
         <img
           src={user.profilePicture}
           alt={user.name}
-          className="w-24 h-24 sm:w-28 sm:h-28 md:w-32 md:h-32 lg:w-36 lg:h-36 rounded-full object-cover"
+          className="w-24 h-24 sm:w-24 sm:h-24 md:w-36 md:h-36 lg:w-36 lg:h-36 rounded-full object-cover"
         />
         <h1 className="text-lg sm:text-xl md:text-2xl lg:text-3xl font-bold mt-4">
           {user.name}
