937995359e272ec9647050dcde2aa3be19e23222
Laurent <saintlaurent414@gmail.com>
2024-09-06 17:07:44 +0100

    React-TAILWIND STYLING

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,34 +1,34 @@
-import React from 'react';
-import RecipeList from './components/RecipeList';
-import FavoritesList from './FavoritesList';
-import RecommendationsList from './RecommendationsList';
-import AddRecipeForm from './components/AddRecipeForm';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import RecipeList from './components/RecipeList';
-import FilterControls from './FilterControls';
-import AddRecipeForm from './components/AddRecipeForm';
-import RecipeDetails from './components/RecipeDetails';
+// import React from 'react';
+// import RecipeList from './components/RecipeList';
+// import FavoritesList from './FavoritesList';
+// import RecommendationsList from './RecommendationsList';
+// import AddRecipeForm from './components/AddRecipeForm';
+// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+// import RecipeList from './components/RecipeList';
+// import FilterControls from './FilterControls';
+// import AddRecipeForm from './components/AddRecipeForm';
+// import RecipeDetails from './components/RecipeDetails';
 
-const App = () => {
-  return (
-    <div>
-      <Router>
-        <div>
-          <h1>Recipe Sharing Application</h1>
-          <SearchBar />
-          <FilterControls />
-          <FavoritesList />
-          <RecommendationsList />
-          <RecipeList />
-          <AddRecipeForm />
-          <Routes>
-            <Route path="/" element={<RecipeList />} />
-            <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
-          </Routes>
-        </div>
-      </Router>
-    </div>
-  );
-};
+// const App = () => {
+//   return (
+//     <div>
+//       <Router>
+//         <div>
+//           <h1>Recipe Sharing Application</h1>
+//           <SearchBar />
+//           <FilterControls />
+//           <FavoritesList />
+//           <RecommendationsList />
+//           <RecipeList />
+//           <AddRecipeForm />
+//           <Routes>
+//             <Route path="/" element={<RecipeList />} />
+//             <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+//           </Routes>
+//         </div>
+//       </Router>
+//     </div>
+//   );
+// };
 
-export default App;
+// export default App;
